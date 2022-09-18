4a438c1d3971bff8cf4484c0fac34cee3b0b4dd3
André Geraldo Guimarães Pinto <italiabk@gmail.com>
2022-09-17 22:57:37 -0400

    build: api.jwt middleware

--- api/routes/api.php
+++ api/routes/api.php
@@ -7,16 +7,17 @@ use Illuminate\Support\Facades\Route;
 
 // Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware('api')->prefix('auth')->name('auth.')->group(function($router) {
+Route::prefix('auth')->name('auth.')->group(function($router) {
 
-        $router->post('login', [AuthController::class, 'login'])->name('login');
-        // $router->post('register', [AuthController::class, 'register'])->name('register');
+    $router->post('login', [AuthController::class, 'login'])->name('login');
+    // $router->post('register', [AuthController::class, 'register'])->name('register');
 
 });
 
-Route::middleware('api')->name('products.')->group(function($router) {
+Route::middleware('api.jwt')->name('products.')->group(function($router) {
 
     $router->apiResource('products', ProductController::class);
+    $router->apiResource('categories', ProductController::class);
 
 });
 
