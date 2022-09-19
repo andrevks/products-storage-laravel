e99f0dae14c57b86d66145192a39d3b790a73c3e
André Geraldo Guimarães Pinto <italiabk@gmail.com>
2022-09-19 11:59:15 -0400

    build: front dashboard list products

--- app/src/pages/dashboard/index.tsx
+++ app/src/pages/dashboard/index.tsx
@@ -1,32 +1,88 @@
+import { Container, Table } from "@mantine/core"
+import { GetServerSideProps } from "next"
 import { getAPIClient } from "../../services/axios"
+import styles from './Dashboard.module.css'
 
-export default function Dashboard(){
+interface IProduct {
+  id: number
+  title: string
+  qty: number
+  unit_price: number
+  category_id: number
+  user_id?: any
+  category: string
+  created_at: Date
+  updated_at: Date
+}
+
+interface IDashboardProps {
+  products: IProduct[]
+}
+
+export default function Dashboard({
+  products
+}:IDashboardProps){
+
+  const rows = products.map((product) => (
+      <tr key={product.id} >
+        <td>{product.title}</td>
+        <td>{product.qty}</td>
+        <td>{product.unit_price}</td>
+        <td>{product.category}</td>
+        <td>{Math.round(product.unit_price * product.qty)}</td>
+      </tr>
+    ))
+  
+  
   return (
-    <h1>Dashboard</h1>
+      <Container className={styles.dashboard}>
+        <h1>Dashboard</h1>
+        <div>
+          { products && 
+            ( 
+              <Table striped highlightOnHover>
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>Quantidade</th>
+                    <th>Valor Unitário</th>
+                    <th>Categoria</th>
+                    <th>Valor Total</th>
+                  </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+
+            </Table>
+            )
+          }   
+        </div>
+      </Container>
   )
 }
 
-export async function getServerSideProps(context: any) {
+export const getServerSideProps: GetServerSideProps =  async ctx => {
 
-  const api = getAPIClient(context);
-  console.log(api);
+  const api = getAPIClient(ctx);
 
   try {
-    const response = await api.get('/products')
-    console.log(response)
-    // console.log(response)
-    // return {
-    //   props: {
-    //     products: response.data.payload
-    //   }
-    // }
+    const { data }: any = await api.get('/products')
+ 
+    return {
+      props: {
+        products: data
+      }
+    }
   } catch (error) {
-    console.log(error);
+    return {
+      props: {}, 
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
   }
 
-  return {
-    props: {
+  
 
-    }, // will be passed to the page component as props
-  }
+ 
 }
